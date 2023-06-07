b66b446575d839e4038473db36738c3c1c59b5fc
Rafael Medeiros <r_mdbarbosa@hotmail.com>
2023-06-06 21:01:25 -0300

    operators functions

---
+++ script.js
@@ -0,0 +1,39 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+function operate(operator, num1, num2) {
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "*") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
+  }
+}
+
+console.log(
+  operate(
+    prompt("choose operator:"),
+    parseInt(prompt("choose num1:")),
+    parseInt(prompt("choose num2:"))
+  )
+);
