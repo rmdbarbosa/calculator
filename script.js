0d7cb7b8a7a066fe7c87d9bd26f3db71981951a5
Rafael Barbosa <69510537+rmdbarbosa@users.noreply.github.com>
2023-06-07 11:47:56 -0300

    I AM COMPLETE

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const operatorButton = document.querySelectorAll(".operator");
+const numberButton = document.querySelectorAll(".number");
+const currentNum = document.querySelector(".current-num");
+const equalsKey = document.querySelector("#equal");
+const clearButton = document.querySelector("#clear");
+const deleteButton = document.querySelector("#delete");
+const pointButton = document.getElementById("point");
+
+pointButton.addEventListener("click", appendPoint);
+deleteButton.addEventListener("click", deleteNumber);
+
+function deleteNumber() {
+  currentNum.textContent = currentNum.textContent.toString().slice(0, -1);
+  storedNumber = currentNum.textContent;
+}
+
+function appendPoint() {
+  if (currentNum.textContent === "" || storedNumber === "")
+    (currentNum.textContent = "0"), (storedNumber = "0");
+
+  if (currentNum.textContent.includes(".")) return;
+  (currentNum.textContent += "."), (storedNumber += ".");
+}
+
+currentNum.textContent = " ";
+
 function add(num1, num2) {
   return num1 + num2;
 }
@@ -18,7 +44,7 @@ let num1 = 0;
 let num2 = 0;
 let operator = "";
 
-function operate(operator, num1, num2) {
+function operate(num1, num2, operator) {
   if (operator === "+") {
     return add(num1, num2);
   } else if (operator === "-") {
@@ -30,10 +56,42 @@ function operate(operator, num1, num2) {
   }
 }
 
-console.log(
-  operate(
-    prompt("choose operator:"),
-    parseInt(prompt("choose num1:")),
-    parseInt(prompt("choose num2:"))
-  )
-);
+let storedNumber = "";
+let clickedOperator = "";
+let firstNumber = "";
+currentNum.textContent = 0;
+
+numberButton.forEach((number) => {
+  number.addEventListener("click", function () {
+    storedNumber += number.value;
+    currentNum.textContent = storedNumber;
+  });
+});
+
+operatorButton.forEach((operator) => {
+  operator.addEventListener("click", function () {
+    firstNumber = storedNumber;
+
+    clickedOperator = operator.textContent;
+
+    storedNumber = "";
+  });
+});
+
+equalsKey.addEventListener("click", function () {
+  result = roundResult(
+    operate(parseFloat(firstNumber), parseFloat(storedNumber), clickedOperator)
+  );
+
+  currentNum.textContent = result;
+
+  storedNumber = result;
+});
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000;
+}
+
+clearButton.addEventListener("click", function () {
+  location.reload();
+});
